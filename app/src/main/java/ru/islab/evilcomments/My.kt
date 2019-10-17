58cfdebba5d733c5bd66f0cd824aa2ac93f6353a
laskova.is <laskova.is@tr-media.ru>
2019-10-17 18:05:02 +0300

    Нормальная вьюха

--- app/src/main/java/ru/islab/evilcomments/My.kt
+++ app/src/main/java/ru/islab/evilcomments/My.kt
@@ -1,6 +1,7 @@
 package ru.islab.evilcomments
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
@@ -14,20 +15,47 @@ import androidx.core.graphics.drawable.toBitmap
 
 class CircleImageView : ImageView {
 
-    private val borderColor = resources.getColor(R.color.transparent1)
-    private val borderColor2 = resources.getColor(R.color.transparent2)
-    private val borderWidth = 4.0f
-    private val borderWidth2 = 6.0f
+    private var borderColor = resources.getColor(R.color.transparent1)
+    private var borderColor2 = resources.getColor(R.color.transparent2)
+    private var borderWidth = 4.0f
+    private var borderWidth2 = 6.0f
 
-    constructor(context: Context) : super(context) {}
+    var p1 = Paint(Paint.ANTI_ALIAS_FLAG)
+    var p2 = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+    constructor(context: Context) : super(context) {
+        init(null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs)
+    }
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
         context,
         attrs,
         defStyle
     ) {
+        init(attrs)
+    }
+
+    private fun init(set: AttributeSet?) {
+
+        if (set == null) return
+
+        var ta = context.obtainStyledAttributes(set, R.styleable.MyCustomView)
+        borderColor = ta.getColor(R.styleable.MyCustomView_border_color1, resources.getColor(R.color.transparent1))
+        borderColor2 = ta.getColor(R.styleable.MyCustomView_border_color2, resources.getColor(R.color.transparent2))
+
+        borderWidth = ta.getDimension(R.styleable.MyCustomView_border_width1, 4f)
+        borderWidth2 = ta.getDimension(R.styleable.MyCustomView_border_width2, 6f)
+
+        p1.setColor(borderColor2)
+        p2.setColor(borderColor)
+
+        p1.strokeWidth = borderWidth
+        p2.strokeWidth = borderWidth2
+
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -36,17 +64,14 @@ class CircleImageView : ImageView {
     }
 
     private fun drawStroke(canvas: Canvas) {
-        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
         val radius = width / 2f
 
-        /* Border paint */
-        paint.color = borderColor2
-        paint.style = Paint.Style.STROKE
-        paint.strokeWidth = borderWidth2
-        canvas.drawCircle(width / 2f, width / 2f, radius - borderWidth * 3, paint)
-        paint.color = borderColor
-        paint.strokeWidth = borderWidth
-        canvas.drawCircle(width / 2f, width / 2f, radius - borderWidth, paint)
+        p1.style = Paint.Style.STROKE
+        p2.style = Paint.Style.STROKE
+
+        canvas.drawCircle(width / 2f, width / 2f, radius - borderWidth * 3, p1)
+
+        canvas.drawCircle(width / 2f, width / 2f, radius - borderWidth, p2)
     }
 
     private fun drawRoundImage(canvas: Canvas) {
@@ -65,7 +90,6 @@ class CircleImageView : ImageView {
             true
         )
 
-        /* Cutting the outer of the circle */
         val shader: Shader
         shader = BitmapShader(scaledBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
 
